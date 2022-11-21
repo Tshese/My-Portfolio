b48a85983710d8814cdde2189076b2c4f665f278
tnthunya <tnthunya021@student.wethinkcode.co.za>
2022-11-21 16:37:29 +0200

    Production ready code

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/odina2.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/tshese-nthunya-587891236"><img src={navIcon1} alt="Icon" /></a>
+             
+            </div>
+            <p>Thank you for attention</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
